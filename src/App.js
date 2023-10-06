1da6434715fc02606f39a553f3ab8a1d4b14c260
KainatAgha <68002210+KainatAgha@users.noreply.github.com>
2023-10-06 11:37:09 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import Counter from './Counter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h2>React Counter App</h2>
+      <Counter />
+    </div>
+  );
+
+  }
+
+export default App;
